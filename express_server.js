87973811a67a9a8e22f5242b70ebe4b57d0b632e
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-15 22:17:46 +0000

    added functionality for url delete and update, redirecting to /urls page after post requests and checkHTTP function

--- express_server.js
+++ express_server.js
@@ -2,11 +2,15 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-
+const morgan = require('morgan');
 const s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
@@ -17,7 +21,6 @@ app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
-
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -31,11 +34,22 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id", (req, res) => {
+  let update = checkHTTP(req.body.update);
+  urlDatabase[req.params.id] = update;
+  res.redirect("/urls"); // will change later
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
-  urlDatabase[shortURL]  = 'http://' + req.body.longURL;
-  // console.log(urlDatabase);
-  res.send(`This is a shortURL ${shortURL} for ${req.body.longURL}`);         // Respond with 'Ok' (we will replace this)
+  let update = checkHTTP(req.body.longURL);
+  urlDatabase[shortURL] = update;
+  res.redirect("/urls"); // will change later
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -45,7 +59,6 @@ app.get("/u/:shortURL", (req, res) => {
   } else {
     res.redirect(longURL);
   }
-  // console.log(res.statusCode);
 });
 
 app.get('/', (req, res) => {
@@ -60,15 +73,19 @@ app.get('/hello', (req, res) => {
   res.end('<html><body>Hello <b>World</b></body></html>\n');
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 function generateRandomString() {
   return Array(6).join().split(',').map(function() {
     return s.charAt(Math.floor(Math.random() * s.length));
   }).join('');
 }
 
+function checkHTTP(input) {
+  if (input.substring(0, 7) !== 'http://' && input.substring(0, 8) !== 'https://') {
+    input = 'http://' + input;
+  }
+  return input;
+}
 
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
