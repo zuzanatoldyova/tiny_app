1171fe6c152dda3f7fb07b1bbacb7b8ee86e0151
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-17 03:05:53 +0000

    working code after refactoring

--- express_server.js
+++ express_server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 let PORT = process.env.PORT || 8080; // default port 8080
+
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
-const bcrypt = require('bcrypt');
-const cookieSession = require('cookie-session');
+const bcrypt = require("bcrypt");
+const cookieSession = require("cookie-session");
 
 const s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+const errorNotLoggedIn = '<html><body>You are not logged in <a href="/login"> Log in here </a></body></html>\n'
+
 const urlDatabase = {
   "b2xVn2": {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -31,128 +34,114 @@ const users = {
   }
 };
 
-const errorNotLoggedIn = '<html><body>You are not logged in <a href="/login"> Log in here </a></body></html>\n'
-
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Middlewares
-app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
-  name: 'session',
-  keys: ['secret'],
+  name: "session",
+  keys: ["secret"]
 }));
 
+function generateRandomString() {
+  return Array(6).join().split(',').map(function() {
+    return s.charAt(Math.floor(Math.random() * s.length));
+  }).join('');
+}
 
-app.get("/urls", (req, res) => {
-  if (req.session.userId) {
-    let userUrls = findUserUrls(req.session.userId);
-    let templateVars = {
-      user: users[req.session.userId],
-      urls: userUrls
+function ensureHTTP(input) {
+  if (input.substring(0, 7) !== "http://" && input.substring(0, 8) !== "https://") {
+    input = "http://" + input;
+  }
+  return input;
+}
+
+function findUserUrls(userId) {
+  let userUrls = {};
+  if (!userId) {
+    return undefined;
+  }
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userId === userId) {
+      userUrls[url] = urlDatabase[url];
+    }
+  }
+  return userUrls;
+}
+
+// helper function / middleware checks existence and authorization to modify url
+
+function checkUrlEdit(req, res, next) {
+  if (!urlDatabase[req.params.id]) {
+    res.status(404).send("URL does not exist");
+  } else if (urlDatabase[req.params.id].userId !== req.session.userId) {
+    res.status(403).send("You are not authorized to modify this URL");
+  } else {
+    next();
+  }
+}
+
+// helper function / middleware checks if is user is logged in
+
+function checkLogin(req, res, next) {
+  if (!req.session.userId) {
+    res.status(401).send(errorNotLoggedIn);
+  } else {
+    next();
+  }
+}
+
+app.get("/urls", checkLogin, (req, res) => {
+  let userUrls = findUserUrls(req.session.userId);
+  let templateVars = {
+    user: users[req.session.userId],
+    urls: userUrls
+  };
+  res.render("urls_index", templateVars);
+});
+
+app.post("/urls", checkLogin, (req, res) => {
+  if (req.body.longURL) {
+    let shortURL = generateRandomString();
+    let longURL = ensureHTTP(req.body.longURL);
+    urlDatabase[shortURL] = {
+      [shortURL]: longURL,
+      userId: req.session.userId
     };
-    res.render('urls_index', templateVars);
+    res.redirect(`/urls/${shortURL}`);
   } else {
-    res.status(401).send(errorNotLoggedIn);
+    res.redirect("/urls/new");
   }
 });
 
-app.post("/urls", (req, res) => {
-  if (req.session.userId) {
-    if (req.body.longURL) {
-      let shortURL = generateRandomString();
-      let longURL = checkHTTP(req.body.longURL);
-      urlDatabase[shortURL] = {
-        [shortURL]: longURL,
-        userId: req.session.userId
-      };
-      res.redirect(`/urls/${shortURL}`);
-    } else {
-      res.redirect("/urls/new");
-    }
-  } else {
-    res.status(401).send(errorNotLoggedIn);
+app.get("/urls/new", checkLogin, (req, res) => {
+  let templateVars = {
+    user: users[req.session.userId]
   }
-});
-
-app.get("/urls/new", (req, res) => {
-  if (req.session.userId) {
-    let templateVars = {
-      user: users[req.session.userId]
-    }
-    res.render("urls_new", templateVars);
-    return;
-  }
-  res.status(401).send(errorNotLoggedIn);
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", checkLogin, checkUrlEdit, (req, res) => {
-
   let templateVars = {
     user: users[req.session.userId],
     shortURL: req.params.id,
     fullURL: urlDatabase[req.params.id][req.params.id]
   };
   res.render("urls_show", templateVars);
-
-  // checkUrlEdit(req, res, showUrl);
-
-  // checkUrlEdit(req, res, (req, res) => {
-  //   let templateVars = {
-  //     user: users[req.session.userId],
-  //     shortURL: req.params.id,
-  //     fullURL: urlDatabase[req.params.id][req.params.id]
-  //   };
-  //   res.render("urls_show", templateVars);
-  // });
-
-  // if (req.session.userId) {
-  //   if (urlDatabase[req.params.id]) {
-  //     if (urlDatabase[req.params.id].userId === req.session.userId) {
-  //       let templateVars = {
-  //         user: users[req.session.userId],
-  //         shortURL: req.params.id,
-  //         fullURL: urlDatabase[req.params.id][req.params.id]
-  //        };
-  //       res.render("urls_show", templateVars);
-  //       return;
-  //     }
-  //     res.status(403).send("You are not authorized to modify this URL");
-  //     return;
-  //   }
-  //   res.status(404).send("URL does not exist");
-  //   return;
-  // }
-  // res.status(401).send(errorNotLoggedIn);
 });
 
-app.post("/urls/:id", (req, res) => {
-  // checkUrlEdit(req, res, updateDatabase(req, res));
-  if (req.session.userId) {
-    if (urlDatabase[req.params.id]) {
-      if (urlDatabase[req.params.id].userId === req.session.userId) {
-        if (req.body.update) {
-          let update = checkHTTP(req.body.update);
-          urlDatabase[req.params.id] = {
-            [req.params.id]: update,
-            userId: req.session.userId
-          };
-          res.redirect("/urls");
-          return;
-        }
-        res.redirect(`/urls/${req.params.id}`);
-        return;
-      }
-      res.status(403).send("You are not authorized to modify this URL");
-      return;
-    }
-    res.status(404).send("URL does not exist");
-    return;
+app.post("/urls/:id", checkLogin, checkUrlEdit, (req, res) => {
+  if (req.body.update) {
+    let update = ensureHTTP(req.body.update);
+    urlDatabase[req.params.id] = {
+      [req.params.id]: update,
+      userId: req.session.userId
+    };
   }
-  res.status(401).send(errorNotLoggedIn);
+  res.redirect(`/urls/${req.params.id}`);
 });
 
-app.post("/urls/:id/delete", (req, res) => {
+app.post("/urls/:id/delete", checkLogin, checkUrlEdit, (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
@@ -194,18 +183,18 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  let mistake = false;
+  let error = false;
   if (!req.body.email || !req.body.password) {
-    mistake = true;
+    error = true;
     res.status(400).end("Email and password are required");
   }
   for (var userId in users) {
       if (users[userId].email === req.body.email) {
-        mistake = true;
+        error = true;
         res.status(400).end("Email already used");
     }
   }
-  if (!mistake) {
+  if (!error) {
     let newUserId = generateRandomString();
     const password = req.body.password;
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -223,7 +212,7 @@ app.get("/register", (req, res) => {
   if (req.session.userId) {
     res.redirect("/");
   } else {
-    let templateVars = {user : null};
+    let templateVars = { user : null };
     res.render("register_form", templateVars);
   }
 });
@@ -231,93 +220,18 @@ app.get("/register", (req, res) => {
 app.get("/", (req, res) => {
   if (req.session.userId) {
     res.redirect("/urls");
-    return;
+  } else {
+    res.redirect("/login");
   }
-  res.redirect("/login");
 });
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.end('<html><body>Hello <b>World</b></body></html>\n');
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
 });
-
-function generateRandomString() {
-  return Array(6).join().split(',').map(function() {
-    return s.charAt(Math.floor(Math.random() * s.length));
-  }).join('');
-}
-
-function checkHTTP(input) {
-  if (input.substring(0, 7) !== 'http://' && input.substring(0, 8) !== 'https://') {
-    input = 'http://' + input;
-  }
-  return input;
-}
-
-function findUserUrls(userId) {
-  let userUrls = {};
-  if (!userId) {
-    return undefined;
-  }
-  for (let url in urlDatabase) {
-    if (urlDatabase[url].userId === userId) {
-      userUrls[url] = urlDatabase[url];
-    }
-  }
-  return userUrls;
-}
-
-// // helper function used in /urls/:id post and get request, checking sign in, existence of url and authorization to modify url
-
-function checkUrlEdit(req, res, next) {
-  // if (!req.session.userId) {
-  //   res.status(401).send(errorNotLoggedIn);
-  // } else
-  if (!urlDatabase[req.params.id]) {
-    res.status(404).send("URL does not exist");
-  } else if (urlDatabase[req.params.id].userId !== req.session.userId) {
-    res.status(403).send("You are not authorized to modify this URL");
-  } else {
-    next();
-  }
-}
-
-function checkLogin(req, res, next) {
-  if (!req.session.userId) {
-    res.status(401).send(errorNotLoggedIn);
-  } else {
-    next();
-  }
-}
-
-// // helper function in /urls/:id send request, updates url in database
-
-// function updateDatabase(req, res) {
-//   if (req.body.update) {
-//     let update = checkHTTP(req.body.update);
-//     urlDatabase[req.params.id] = {
-//       [req.params.id]: update,
-//       userId: req.session.userId
-//     };
-//     res.redirect("/urls");
-//   } else {
-//   res.redirect(`/urls/${req.params.id}`);
-//   }
-// }
-
-// // helper function in /urls/:id get request, setts parameters and render approtpriate url
-
-function showUrl(req, res) {
-  let templateVars = {
-    user: users[req.session.userId],
-    shortURL: req.params.id,
-    fullURL: urlDatabase[req.params.id][req.params.id]
-  };
-  res.render("urls_show", templateVars);
-}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
