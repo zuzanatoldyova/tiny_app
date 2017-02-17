31b1b235c1f42805b3cf15b792713737fc0fe1f8
zuzanatoldyova <zuzana.toldyova@gmail.com>
2017-02-17 00:02:42 +0000

    code modified according to technical specifications

--- express_server.js
+++ express_server.js
@@ -45,7 +45,6 @@ app.use(cookieParser());
 app.get("/urls", (req, res) => {
   if (req.cookies.userId) {
     let userUrls = findUserUrls(req.cookies.userId);
-    console.log(userUrls);
     let templateVars = {
       user: users[req.cookies.userId],
       urls: userUrls
@@ -57,16 +56,21 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  if (req.body.longURL) {
-    let shortURL = generateRandomString();
-    let longURL = checkHTTP(req.body.longURL);
-    urlDatabase[shortURL] = {
-      [shortURL]: longURL,
-      userId: req.cookies.userId
-    };
-    res.redirect("/urls");
+  if (req.cookies.userId) {
+    if (req.body.longURL) {
+      let shortURL = generateRandomString();
+      let longURL = checkHTTP(req.body.longURL);
+      urlDatabase[shortURL] = {
+        [shortURL]: longURL,
+        userId: req.cookies.userId
+      };
+      console.log(shortURL);
+      res.redirect(`/urls/${shortURL}`);
+    } else {
+      res.redirect("/urls/new");
+    }
   } else {
-    res.redirect("/urls/new");
+    res.status(401).send(errorNotLoggedIn);
   }
 });
 
@@ -82,6 +86,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
+  // checkUrlEdit(req, res, showUrl(req, res));
   if (req.cookies.userId) {
     if (urlDatabase[req.params.id]) {
       if (urlDatabase[req.params.id].userId === req.cookies.userId) {
@@ -103,16 +108,29 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.post("/urls/:id", (req, res) => {
-  if (req.body.update) {
-    let update = checkHTTP(req.body.update);
-    urlDatabase[req.params.id] = {
-      [req.params.id]: update,
-      userId: req.cookies.userId
-    };
-    res.redirect("/urls");
-  } else {
-    res.redirect(`/urls/${req.params.id}`);
+  // checkUrlEdit(req, res, updateDatabase(req, res));
+  if (req.cookies.userId) {
+    if (urlDatabase[req.params.id]) {
+      if (urlDatabase[req.params.id].userId === req.cookies.userId) {
+        if (req.body.update) {
+          let update = checkHTTP(req.body.update);
+          urlDatabase[req.params.id] = {
+            [req.params.id]: update,
+            userId: req.cookies.userId
+          };
+          res.redirect("/urls");
+          return;
+        }
+        res.redirect(`/urls/${req.params.id}`);
+        return;
+      }
+      res.status(403).send("You are not authorized to modify this URL");
+      return;
+    }
+    res.status(404).send("URL does not exist");
+    return;
   }
+  res.status(401).send(errorNotLoggedIn);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
@@ -121,7 +139,11 @@ app.post("/urls/:id/delete", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  res.render("login_form"); //or home?
+  if (req.cookies.userId) {
+    res.redirect("/");
+  } else {
+    res.render("login_form"); //or home?
+  }
 });
 
 app.post("/login", (req, res) => {
@@ -129,12 +151,12 @@ app.post("/login", (req, res) => {
     if (req.body.email === users[user].email) {
       if (bcrypt.compareSync(req.body.password, users[user].password)) {
         res.cookie("userId", user);
-          res.redirect("/"); //or home?
+        res.redirect("/"); //or home?
         return;
       }
     }
   }
-  res.status(403).send("Invalid credentials.");
+  res.status(401).send("Invalid credentials.");
 });
 
 app.post("/logout", (req, res) => {
@@ -145,7 +167,7 @@ app.post("/logout", (req, res) => {
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL][req.params.shortURL];
   if (!longURL) {
-    res.status(404).end('<html><body>url does not exist</body></html>\n');
+    res.status(404).end('<html><body>Url does not exist</body></html>\n');
   } else {
     res.redirect(longURL);
   }
@@ -172,14 +194,17 @@ app.post("/register", (req, res) => {
       email: req.body.email,
       password: hashedPassword
     };
-    console.log(users);
     res.cookie("userId", newUserId);
     res.redirect("/");
   }
 });
 
 app.get("/register", (req, res) => {
-  res.render("register_form")
+  if (req.cookies.userId) {
+    res.redirect("/");
+  } else {
+    res.render("register_form");
+  }
 });
 
 app.get("/", (req, res) => {
@@ -217,13 +242,53 @@ function findUserUrls(userId) {
     return undefined;
   }
   for (let url in urlDatabase) {
-    console.log(urlDatabase[url].userId);
     if (urlDatabase[url].userId === userId) {
       userUrls[url] = urlDatabase[url];
     }
   }
   return userUrls;
 }
+
+// // helper function used in /urls/:id post and get request, checking sign in, existence of url and authorization to modify url
+
+// function checkUrlEdit(req, res, callback) {
+//   if (!req.cookies.userId) {
+//     res.status(401).send(errorNotLoggedIn);
+
+//   } else if (!urlDatabase[req.params.id]) {
+//     res.status(404).send("URL does not exist");
+//   } else if (! urlDatabase[req.params.id].userId === req.cookies.userId) {
+//     res.status(403).send("You are not authorized to modify this URL");
+//   } else {
+//     callback;
+//   }
+// }
+
+// // helper function in /urls/:id send request, updates url in database
+
+// function updateDatabase(req, res) {
+//   if (req.body.update) {
+//     let update = checkHTTP(req.body.update);
+//     urlDatabase[req.params.id] = {
+//       [req.params.id]: update,
+//       userId: req.cookies.userId
+//     };
+//     res.redirect("/urls");
+//   } else {
+//   res.redirect(`/urls/${req.params.id}`);
+//   }
+// }
+
+// // helper function in /urls/:id get request, setts parameters and render approtpriate url
+
+// function showUrl(req, res) {
+//   let templateVars = {
+//     user: users[req.cookies.userId],
+//     shortURL: req.params.id,
+//     fullURL: urlDatabase[req.params.id][req.params.id]
+//   };
+//   res.render("urls_show", templateVars);
+// }
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
